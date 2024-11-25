b9a5ca156e94c9438f695476cb6e8f20fdf4dbe8
Joaquín Pozo <joaquin.pozo@usach.cl>
2024-11-25 16:33:25 -0300

    Implementación correcta de lectura de archivos.

--- assets/lectura_archivos.c
+++ assets/lectura_archivos.c
@@ -1,28 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../TDA_planificacion/TDA_planificacion.h"
-void *leerArchivo (const char* nombreArchivo, int *cantidadCargas, int *cantidadProcesos) {
+int **leerArchivo (char nombreArchivo[], int *cantidadCargas, int *cantidadProcesos) {
     FILE *archivo = fopen(nombreArchivo, "r");
 
+    // Comprueba el nombre del archivo ingresado
+    /*
+    if (archivo == NULL) {
+    	printf("Error al abrir archivo %s\n", nombreArchivo);
+        return NULL;
+	}
+    */
+    if (archivo == NULL) {
+    fprintf(stderr, "Error: El archivo '%s' no existe en la carpeta 'assets'. Verifica el nombre e intenta nuevamente.\n", nombreArchivo);
+    exit(EXIT_FAILURE);
+    }   
+    
     // Lee la cantidad de cargas y procesos
     fscanf(archivo, "%d %d", cantidadCargas, cantidadProcesos);
 
-    // Limpia el salto de linea en el bufer
-    fgetc(archivo);
+    // Crea un arreglo dinamico para las cargas
+    int **cargasIniciales = (int**)malloc(*cantidadCargas * sizeof(int*));
 
+    // Lectura de los datos de cada carga
     for (int i = 0; i < *cantidadCargas; i++) {
-        fgets(linea, sizeof(linea), archivo);
-        sscanf(linea,"%d %d %d %d %d %d",[i]);
+        // Crea un arreglo dinámico para cada carga
+        cargasIniciales[i] = (int*)malloc(2 * (*cantidadProcesos) * sizeof(int));
+        // Lee los datos de cada carga por linea correspondiente
+        for (int j = 0; j < 2 * (*cantidadProcesos); j++) {
+            fscanf(archivo, "%d", &cargasIniciales[i][j]);
+        }
     }
-
-    // Crea un arreglo dinámico para procesos
-    for (int i = 0; i < cantidadProcesos; i++) {
-        proceso *p_i = crea_proceso(i, cantidadCargas);
-    }
-    char linea[256];
-    
-    
-
     fclose(archivo);
+    return cargasIniciales;
+}
 
+void liberarMemoria(int **cargasIniciales, int cantidadCargas) {
+    for (int i = 0; i < cantidadCargas; i++) {
+        free(cargasIniciales[i]);
+    }
+    free(cargasIniciales);
+}
+
+void imprimirCargas(int **cargasIniciales, int cantidadCargas, int cantidadProcesos) {
+    for (int i = 0; i < cantidadCargas; i++) {
+        printf("Carga %d:\n", i + 1);
+        for (int j = 0; j < cantidadProcesos; j++) {
+            printf("  Proceso %d: Tiempo %d\n", 
+                   cargasIniciales[i][2 * j], // id
+                   cargasIniciales[i][2 * j + 1]); // tiempo
+        }
+    }
 }
--- procesamiento_cargas.c
+++ procesamiento_cargas.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "TDA_carga/TDA_carga.h"
 #include "TDA_proceso/TDA_proceso.h"
 #include "TDA_planificacion/TDA_planificacion.h"
+#include "assets/lectura_archivos.c"
 /*
 Para ejecutar el main, se debe usar esta linea en la consola de comandos:
 gcc procesamiento_cargas.c TDA_carga/TDA_carga_implementacion.c TDA_proceso/TDA_proceso_implementacion.c TDA_planificacion/TDA_planificacion_implementacion.c -o cargas
 */
 
-int main() {
+// nombre archivo, nombre archivo que desea leer
+int main(int argc, char *argv[]) {
     
-    carga *c1_1 = crea_carga(1, 1, 3);
-    carga *c1_2 = crea_carga(1, 2, 3);
-    carga *c1_3 = crea_carga(1, 3, 3);
+    char nombreArchivo[256];
+
+    snprintf(nombreArchivo, sizeof(nombreArchivo), "assets/%s", argv[1]);
+
+    int cantidadCargas, cantidadProcesos;
+
+    int **cargasIniciales = leerArchivo(nombreArchivo, &cantidadCargas, &cantidadProcesos);
+
+    printf("Datos de las cargas:\n");
+    imprimirCargas(cargasIniciales, cantidadCargas, cantidadProcesos);
+
+    liberarMemoria(cargasIniciales, cantidadCargas);
+
+
     
-    /*
-    carga *c2_1 = crea_carga(2, 1, 2);
-    carga *c2_2 = crea_carga(2, 3, 3);
-    carga *c2_3 = crea_carga(2, 2, 4);
-
-    carga *c3_1 = crea_carga(3, 2, 3);
-    carga *c3_2 = crea_carga(3, 1, 2);
-    carga *c3_3 = crea_carga(3, 3, 1);
-    */
-    proceso *p1 = crea_proceso(1, 3);
-    agregar_carga(p1, c1_1);
-    agregar_carga(p1, c1_2);
-    agregar_carga(p1, c1_3);
-
-    planificacion *pl = crea_planificacion(3);
-
-    agregar_proceso(pl, p1);
-
-    mostrar_planificacion(pl);
-
-    destruye_planificacion(pl);
-
-    destruye_proceso(p1);
 
 
     return 0;
