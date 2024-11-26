4b0d7b37386755ee85990885bda491eb32a89ff5
Joaquín Pozo <joaquin.pozo@usach.cl>
2024-11-26 15:56:58 -0300

    Avance en generar conjunto de soluciones

--- procesamiento_cargas.c
+++ procesamiento_cargas.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "TDA_carga/TDA_carga.h"
-#include "TDA_proceso/TDA_proceso.h"
-#include "TDA_planificacion/TDA_planificacion.h"
 #include "assets/lectura_archivos.c"
 /*
 Para ejecutar el main, se debe usar esta linea en la consola de comandos:
-gcc procesamiento_cargas.c TDA_carga/TDA_carga_implementacion.c TDA_proceso/TDA_proceso_implementacion.c TDA_planificacion/TDA_planificacion_implementacion.c -o cargas
+gcc procesamiento_cargas.c -o cargas
 */
 
 // nombre archivo, nombre archivo que desea leer
 int main(int argc, char *argv[]) {
     
-    char nombreArchivo[256];
+    char nombreArchivo[80];
 
+    // Concatena la ruta del archivo con su nombre
     snprintf(nombreArchivo, sizeof(nombreArchivo), "assets/%s", argv[1]);
 
     int cantidadCargas, cantidadProcesos;
@@ -22,16 +20,27 @@ int main(int argc, char *argv[]) {
     int **cargasIniciales = leerArchivo(nombreArchivo, &cantidadCargas, &cantidadProcesos);
 
     printf("Datos de las cargas:\n");
+
     imprimirCargas(cargasIniciales, cantidadCargas, cantidadProcesos);
 
     liberarMemoria(cargasIniciales, cantidadCargas);
 
+    // Almacena memoria para un conjunto con todas las soluciones de O ~ (m * n!)
+    int cantidadSoluciones = cantidadProcesos * factorial(cantidadCargas);
+    int ***conjuntoSoluciones = (int***)malloc(cantidadSoluciones * sizeof(int**));
 
-    // crear conjunto con todas las soluciones
+    // Crea el conjunto con todas las soluciones;
+    for (int i = 0; i < cantidadSoluciones; i++) {
+        conjuntoSoluciones[i] = (int**)malloc(cantidadCargas * sizeof(int*));
+        for (int j = 0; j < cantidadCargas; j++) {
+            conjuntoSoluciones[i][j] = (int*)malloc(2 * (cantidadProcesos) * sizeof(int));
+            
+        }
+    }
 
 
 
-    // aplicar restricciones al conjunto de soluciones
+    // Aplicar restricciones al conjunto de soluciones
 
 
     return 0;
